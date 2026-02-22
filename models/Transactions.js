ec3730e89c4861b81b940313ac68ccdf4838e11a
dishasharma23-prog <disha.sharma23@st.niituniversity.in>
2026-02-22 22:45:40 +0530

    Delete models directory

--- models/Transactions.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const transactionSchema = new mongoose.Schema({
-    Amount: Number,
-    is_fraud: Number,
-    riskScore: Number,
-    timestamp: { type: Date, default: Date.now }
-}, { strict: false });
-module.exports = mongoose.model('Transaction', transactionSchema, 'predictions');
